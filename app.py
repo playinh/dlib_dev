bd2c4d324ee458b3fa0d2c6d2185adc59138d9bf
John Andrew Sombilon <133199392+playinh@users.noreply.github.com>
2023-11-14 17:15:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ class VideoProcessor:
         	s = predictor(gray, rect)
         	s = face_utils.shape_to_np(s)
 
-        	#for(i, y) in s:
-        		#cv2.circle(img, (i,y), 2, (0, 255, 0), -1)"'
+        	for(i, y) in s:
+        		cv2.circle(img, (i,y), 2, (0, 255, 0), -1)"'
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
